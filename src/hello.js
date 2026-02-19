f03f90cd45798a6fc7092af035d71662bebbdf57
Peter Clarke <clarke.peterf@gmail.com>
2026-02-19 09:09:20 -0500

    update hello.js to read from terminal

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,27 @@
+const readline = require("readline");
+
 function hello(name) {
   return `Hello, ${name}!`;
 }
 
-console.log(hello("World"));
+const questions = ["What is your name?"];
+const answers = [];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askQuestion(i) {
+  if (i < questions.length) {
+    rl.question(questions[i] + " ", (answer) => {
+      answers.push(answer);
+      askQuestion(i + 1);
+    });
+  } else {
+    console.log(hello(...answers));
+    rl.close();
+  }
+}
+
+askQuestion(0);
